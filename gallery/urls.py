d2cee1387902aaad572b9782725c7584a03654d1
brayooh <brianndichu.bn@gmail.com>
2019-11-10 23:56:14 +0300

    add search url

--- gallery/urls.py
+++ gallery/urls.py
@@ -6,5 +6,5 @@ from django.conf.urls import url
 
 urlpatterns=[
     url(r'^$', views.posted_images, name='home'),
-    url(r'^search/', views.search_results, name='search_results')
+    url(r'^search/', views.search_results,name='search_results')
 ]
