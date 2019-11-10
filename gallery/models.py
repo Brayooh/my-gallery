080ed96ed687088d83f58a341fe4dbd769afd2f3
brayooh <brianndichu.bn@gmail.com>
2019-11-10 18:07:10 +0300

    add class methods to image class

--- gallery/models.py
+++ gallery/models.py
@@ -1,17 +1,7 @@
 from django.db import models
 
-class image(models.Model):
-    image = models.ImageField(upload_to='images')
-    image_name = models.CharField(max_length=50)
-    caption = models.CharField()
-    image_location = models.CharField(max_length=60)
-    category = models.ForeignKey(category, on_delete=models.CASCADE)
-    pub_date = models.DateTimeField(auto_now_add=True)
-    tags = models.ManyToManyField(tags)
 
-
-
-class category(models.Model):
+class Category(models.Model):
     name = models.CharField(max_length=30)
 
 
@@ -23,3 +13,37 @@ class tags(models.Model):
 
     def __str__(self):
         return self.name
+
+class Location(models.Model):
+    name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name
+
+
+
+class image(models.Model):
+    image = models.ImageField(upload_to='images')
+    image_name = models.CharField(max_length=50)
+    caption = models.CharField(max_length=100)
+    location = models.ForeignKey(Location, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    pub_date = models.DateTimeField(auto_now_add=True)
+    tags = models.ManyToManyField(tags)
+
+
+     @classmethod
+    def search_by_title(cls, search_term):
+        image = cls.objects.filter(description__contains=search_term)
+
+        return image
+
+    @classmethod
+    def get_image_by_id(cls, id):
+        return cls.objects.get(pk=id)
+
+   
+
+
+
+
