f435e7dfa459335c3bb4f1c3bad896f57a559551
brayooh <brianndichu.bn@gmail.com>
2019-11-11 02:40:05 +0300

    modify search function

--- gallery/views.py
+++ gallery/views.py
@@ -5,15 +5,15 @@ from .models import image
 
 def posted_images(request):
 
-    pic = image.object.all()
+    pics = image.objects.all()
 
-    return render(request, 'index.html', {"pic":pic})
+    return render(request, 'index.html', {"pics":pics})
 
 
 def search_results(request):
     if 'pictures' in request.GET and request.GET["pictures"]:
         searched_term = request.GET.get("pictures")
-        searched_pictures = Picture.search_by_title(searched_term)
+        searched_pictures = image.search_by_title(searched_term)
         message = f"{searched_term}"
 
         return render(request, 'pic/search.html', {"message": message, "pictures": searched_pictures})
