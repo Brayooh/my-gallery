e7aac19401eb2f0a2180cf4056d98107d54ba6e9
brayooh <brianndichu.bn@gmail.com>
2019-11-11 02:40:42 +0300

    change caption field to TextField

--- gallery/models.py
+++ gallery/models.py
@@ -25,14 +25,14 @@ class Location(models.Model):
 class image(models.Model):
     image = models.ImageField(upload_to='images')
     image_name = models.CharField(max_length=50)
-    caption = models.CharField(max_length=100)
+    caption = models.TextField()
     location = models.ForeignKey(Location, on_delete=models.CASCADE)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     pub_date = models.DateTimeField(auto_now_add=True)
     tags = models.ManyToManyField(tags)
 
 
-     @classmethod
+    @classmethod
     def search_by_title(cls, search_term):
         image = cls.objects.filter(description__contains=search_term)
 
