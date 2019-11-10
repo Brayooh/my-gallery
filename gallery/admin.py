ea24aeca5a8dbfa1d1c54aedb13b21628c4fa872
brayooh <brianndichu.bn@gmail.com>
2019-11-10 23:57:42 +0300

    register tables to admin + filter tags horizontally

--- gallery/admin.py
+++ gallery/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import image,Category,tags, Location
 
-# Register your models here.
+class ArticleAdmin(admin.ModelAdmin):
+    filter_horizontal =('tags',)
+
+admin.site.register(image)
+admin.site.register(Category)
+admin.site.register(Location)
+admin.site.register(tags)
