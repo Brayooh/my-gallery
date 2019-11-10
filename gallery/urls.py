8afdc30fc1f6b71f5334d44456b33fe4cd29ad84
brayooh <brianndichu.bn@gmail.com>
2019-11-11 02:39:18 +0300

    create media url pattern

--- gallery/urls.py
+++ gallery/urls.py
@@ -1,10 +1,14 @@
 from django.conf import settings
 from . import views
 from django.conf.urls import url
+from django.conf.urls.static import static
 
 
 
 urlpatterns=[
     url(r'^$', views.posted_images, name='home'),
-    url(r'^search/', views.search_results,name='search_results')
+    url(r'^search/', views.search_results,name='search_results'), 
 ]
+
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
