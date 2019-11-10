3e54e68744e15ccee0c0ff71e1b73dffe3239932
brayooh <brianndichu.bn@gmail.com>
2019-11-10 17:17:15 +0300

    add category, tags and image classes

--- gallery/models.py
+++ gallery/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-class 
+class image(models.Model):
+    image = models.ImageField(upload_to='images')
+    image_name = models.CharField(max_length=50)
+    caption = models.CharField()
+    image_location = models.CharField(max_length=60)
+    category = models.ForeignKey(category, on_delete=models.CASCADE)
+    pub_date = models.DateTimeField(auto_now_add=True)
+    tags = models.ManyToManyField(tags)
+
+
+
+class category(models.Model):
+    name = models.CharField(max_length=30)
+
+
+    def __str__(self):
+        return self.name
+
+class tags(models.Model):
+    name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name
