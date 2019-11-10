dcc47ce707e793decd63284421a6821fcac1d8e7
brayooh <brianndichu.bn@gmail.com>
2019-11-10 23:59:06 +0300

    add search function

--- gallery/views.py
+++ gallery/views.py
@@ -9,3 +9,16 @@ def posted_images(request):
 
     return render(request, 'index.html', {"pic":pic})
 
+
+def search_results(request):
+    if 'pictures' in request.GET and request.GET["pictures"]:
+        searched_term = request.GET.get("pictures")
+        searched_pictures = Picture.search_by_title(searched_term)
+        message = f"{searched_term}"
+
+        return render(request, 'pic/search.html', {"message": message, "pictures": searched_pictures})
+
+    else:
+        message = "You have not searched for any picture"
+        return render(request, 'pic/search.html', {"message": message})
+
