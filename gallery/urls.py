4775fdf67e3db17e6b4db39076efbb1b3d2e38e1
brayooh <brianndichu.bn@gmail.com>
2019-11-10 16:44:39 +0300

    create app gallery

---
+++ gallery/urls.py
@@ -0,0 +1,10 @@
+from django.conf import settings
+from . import views
+from django.conf import urls
+
+
+
+urlpatterns=[
+    url(r'^$', views.posted_images, name='home'),
+    url(r'^search/', views.search_results, name='search_results')
+]
