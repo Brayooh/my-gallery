d0ad71e8159bc5fc255d0cbf07506de53c29f0e5
brayooh <brianndichu.bn@gmail.com>
2019-11-10 17:25:12 +0300

    create images view function and return html

--- gallery/views.py
+++ gallery/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import image
 
 
 def posted_images(request):
-    
+
+    pic = image.object.all()
+
+    return render(request, 'index.html', {"pic":pic})
 
