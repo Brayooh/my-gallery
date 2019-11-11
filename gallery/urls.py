e4e375729a4024b2f49a8c0d409ebd5cf3a6eb93
brayooh <brianndichu.bn@gmail.com>
2019-11-11 15:57:00 +0300

    modify url patterns

--- gallery/urls.py
+++ gallery/urls.py
@@ -2,13 +2,18 @@ from django.conf import settings
 from . import views
 from django.conf.urls import url
 from django.conf.urls.static import static
+from django.contrib import admin
 
 
 
 urlpatterns=[
     url(r'^$', views.posted_images, name='home'),
     url(r'^search/', views.search_results,name='search_results'), 
+   
 ]
 
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+
+# admin.site.add = 'admin/add_image.html'
+# admin.autodiscover()
