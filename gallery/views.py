3bbffa59f4f6c00e392af61b206dc18e1347b6c5
brayooh <brianndichu.bn@gmail.com>
2019-11-11 15:57:42 +0300

    modify search function to filter by category

--- gallery/views.py
+++ gallery/views.py
@@ -9,16 +9,28 @@ def posted_images(request):
 
     return render(request, 'index.html', {"pics":pics})
 
+# def add_image(request):
+
+
+#     return render(request, 'add_image.html')
+
 
 def search_results(request):
     if 'pictures' in request.GET and request.GET["pictures"]:
         searched_term = request.GET.get("pictures")
-        searched_pictures = image.search_by_title(searched_term)
+        searched_pictures = image.search_by_category(searched_term)
         message = f"{searched_term}"
 
-        return render(request, 'pic/search.html', {"message": message, "pictures": searched_pictures})
+        return render(request, 'search.html', {"message": message, "pictures": searched_pictures})
 
     else:
         message = "You have not searched for any picture"
-        return render(request, 'pic/search.html', {"message": message})
+        return render(request, 'search.html', {"message": message})
 
+
+# def copy_to_clipboard(request):
+    
+#     pyperclip.copy('The text to be copied to the clipboard.')
+#     spam = pyperclip.paste()
+
+#     return copy
