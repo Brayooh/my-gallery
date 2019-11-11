3595ae64a5fdfd5fe7e5edb65ced6fc028af6393
brayooh <brianndichu.bn@gmail.com>
2019-11-11 18:12:59 +0300

    deployment changes

--- media/settings.py
+++ media/settings.py
@@ -82,7 +82,7 @@ WSGI_APPLICATION = 'media.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'tribune',
+        'NAME': 'project_g',
         'USER': 'brayooh',
         'PASSWORD':'brian',
     }
@@ -129,12 +129,11 @@ USE_TZ = True
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-
-STATIC_ROOT  =   os.path.join(PROJECT_ROOT, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
+]
 
 
 
